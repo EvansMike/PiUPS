af89b1a7011994aa0448101d21304bb94965ae27
Mike Evans <mikee@saxicola.co.uk>
2018-08-09 16:21:01 +0100

    Bug#196
    Working. Could be better. Perhaps 2 callbacks, rising & falling.

--- firmware/src/powermon.py
+++ firmware/src/powermon.py
@@ -1,58 +1,63 @@
 #!/usr/bin/env python
-# Write a systemd service to start this on boot.
-#
+'''
+ TODO: Write a systemd service to start this on boot.
+'''
 
 import os
 import time
 import RPi.GPIO as GPIO
+# tell the GPIO module that we want to use
+# the chip's pin numbering scheme
+
+GPIO.setmode(GPIO.BCM)
 
 # Pin definitions
 POWER_WAIT = 30
 POWER_LOSS = 22
 RELAY = 27
-
-p = GPIO.PWM(27, 2000)
+p = None
+t_shutdown = None
 
 # handle the power change event
 def signal_power_change_handler (pin):
+    global p
+    global t_shutdown
     timestr = time.strftime("%Y-%m-%d %H:%M:%S")
-    p.start(50) # Power saving, 50%, for relay hold on
-    os.system("logger Power loss at " + timestr)
+    #p.start(50) # Power saving, 50%, for relay hold on
     # Loop for 30s then check if the power is still off
-    t_shutdown = time.time() + POWER_WAIT
-    while time.time() < t_shutdown:
-        pass
-    if GPIO.input(22) == 0: # Check the pin state
-        # If it's still off, order a shut down
-        timestr = time.strftime("%Y-%m-%d %H:%M:%S")
-        os.system("logger UPS ordered power down at " + timestr)
-        os.system("sudo shutdown -h now")
-        GPIO,cleanup()
-        quit(0)
-    else:
-        timestr = time.strftime("%Y-%m-%d %H:%M:%S")
-        p.stop() # Back to just ON
-        GPIO.output(27,True)
-        os.system("logger Power restore at " + timestr)
-        return
+    if GPIO.input(22) == 1 and not t_shutdown:
+        os.system("logger Power loss at " + timestr)
+        t_shutdown = time.time() + POWER_WAIT
+        while time.time() < t_shutdown: # Loop for a bit
+            time.sleep(1)
+            pass
+        if GPIO.input(22) == 1: # Check the pin state again
+            # If it's still high, order a shut down
+            timestr = time.strftime("%Y-%m-%d %H:%M:%S")
+            os.system("logger UPS ordered power down at " + timestr)
+            time.sleep(1)
+            os.system("sudo shutdown -h now")
+            GPIO,cleanup()
+            quit(0)
+        else:
+            timestr = time.strftime("%Y-%m-%d %H:%M:%S")
+            t_shutdown = None
+            #p.stop() # Back to just ON
+            GPIO.output(27,True)
+            os.system("logger Power restore at " + timestr)
+            return
 
 
 # main function
 def main():
+    global p
     timestr = time.strftime("%Y-%m-%d %H:%M:%S")
     os.system("logger UPS system up at " + timestr)
     # Wait a bit for power to settle down.
     time.sleep(5)
-    # tell the GPIO module that we want to use
-    # the chip's pin numbering scheme
-    GPIO.setmode(GPIO.BCM)
-
-    # setup pin 23 as an input
-    # and set up pins 24 and 25 as outputs
     GPIO.setup(22,GPIO.IN)
     GPIO.setup(27,GPIO.OUT)
-
-
+    #p = GPIO.PWM(27, 2000)
     # tell the GPIO library to look out for an
     # event on pin 15 and deal with it by calling
     # the handler function
@@ -60,7 +65,9 @@ def main():
     GPIO.add_event_detect(22,GPIO.BOTH, callback=signal_power_change_handler, bouncetime=30)
     # turn ON the UPS control
     GPIO.output(27,True)
-
+    while 1:
+        time.sleep(1)
+    GPIO.cleanup()
 
 if __name__=="__main__":
     main()
