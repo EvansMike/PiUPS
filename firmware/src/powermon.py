8b7ad6bbdd9739c8c936d574fe0bcdc8b1e193a5
Mike Evans <mikee@saxicola.co.uk>
2018-12-24 18:50:19 +0000

    Remove timestamps in log messages.
    Messages are stamped by the system.

--- firmware/src/powermon.py
+++ firmware/src/powermon.py
@@ -22,29 +22,26 @@ t_shutdown = None
 def signal_power_change_handler(pin):
     global p
     global t_shutdown
-    timestr = time.strftime("%Y-%m-%d %H:%M:%S")
     #p.start(50) # Power saving, 50%, for relay hold on
     # Loop for 30s then check if the power is still off
     if GPIO.input(22) == 1 and not t_shutdown:
-        os.system("logger Power loss at " + timestr)
+        os.system("logger Power lost")
         t_shutdown = time.time() + POWER_WAIT
         while time.time() < t_shutdown: # Loop for a bit
             time.sleep(1)
             pass
         if GPIO.input(22) == 1: # Check the pin state again
             # If it's still high, order a shut down
-            timestr = time.strftime("%Y-%m-%d %H:%M:%S")
-            os.system("logger UPS ordered power down at " + timestr)
+            os.system("logger UPS ordered power down")
             time.sleep(1)
             os.system("sudo shutdown -h now")
             GPIO,cleanup()
             quit(0)
         else:
-            timestr = time.strftime("%Y-%m-%d %H:%M:%S")
             t_shutdown = None
             #p.stop() # Back to just ON
             GPIO.output(27,True)
-            os.system("logger Power restore at " + timestr)
+            os.system("logger Power restored")
             return
 
 
@@ -79,5 +76,3 @@ def main():
 
 if __name__=="__main__":
     main()
-
-
